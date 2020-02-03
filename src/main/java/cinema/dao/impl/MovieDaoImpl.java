26a64951bfc797211c5f189e9ece20caae6dfd19
Mike Podoliaka <mykhailo.podoliaka@gmail.com>
2020-02-03 21:25:04 +0200

    first commit

---
+++ src/main/java/cinema/dao/impl/MovieDaoImpl.java
@@ -0,0 +1,46 @@
+package cinema.dao.impl;
+
+import cinema.dao.MovieDao;
+import cinema.lib.Dao;
+import cinema.model.Movie;
+
+import cinema.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import javax.persistence.criteria.CriteriaQuery;
+
+import java.util.List;
+
+@Dao
+public class MovieDaoImpl implements MovieDao {
+
+    public Movie add(Movie movie) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            Long itemId = (Long) session.save(movie);
+            transaction.commit();
+            movie.setId(itemId);
+            return (movie);
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert Movie entity", e);
+        }
+
+    }
+
+    public List<Movie> getAll() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            CriteriaQuery<Movie> criteriaQuery = session.getCriteriaBuilder()
+                    .createQuery(Movie.class);
+            criteriaQuery.from(Movie.class);
+            return session.createQuery(criteriaQuery).getResultList();
+        } catch (Exception e) {
+           throw new RuntimeException("Error from getAll", e);
+        }
+
+    }
+}
---
+++ src/main/java/cinema/service/MovieService.java
@@ -0,0 +1,12 @@
+package cinema.service;
+
+import cinema.model.Movie;
+
+import java.util.List;
+
+public interface MovieService {
+    Movie add(Movie movie);
+
+    List<Movie> getAll();
+
+}
